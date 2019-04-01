d97b1d9b2baf0c99ed15dffb15e1f4341855c328
qtgeo1248 <gzhou00@stuy.edu>
2019-04-01 18:28:34 -0400

    forgot semicolons

--- Radix.java
+++ Radix.java
@@ -18,18 +18,12 @@ public class Radix {
             buckets[i] = new MyLinkedList<Integer>();
         }
         for (int i = 0; i < data.length; i++) {
-            int digit = Math.abs(data[i] % 10)
+            int digit = Math.abs(data[i] % 10);
             if (data[i] >= 0) {
                 buckets[digit + 10].add(data[i]);
             } else {
                 buckets[9 - digit].add(data[i]);
             }
-        }
-        while (numDigs > 0) {
-            for (int i = 0; i < data.length; i++) {
-
-            }
-            numDigs--;
         }
     }
 }
