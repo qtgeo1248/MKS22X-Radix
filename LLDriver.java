aa2e959604a41c2cd5e51a410cbde81f911be3bf
qtgeo1248 <gzhou00@stuy.edu>
2019-04-01 11:31:23 -0400

    finished testing

--- LLDriver.java
+++ LLDriver.java
@@ -1,5 +1,24 @@
 public class LLDriver {
     public static void main(String[] args) {
-        
+        MyLinkedList<Integer> t1 = new MyLinkedList<Integer>();
+        t1.add(4);
+        t1.add(3);
+        System.out.println(t1);
+        t1.clear();
+        System.out.println(t1);
+        t1.add(5);
+        t1.add(6);
+        t1.removeFront();
+        t1.add(7);
+        System.out.println(t1);
+
+        MyLinkedList<Integer> t2 = new MyLinkedList<Integer>();
+        t2.add(7);
+        t2.add(8);
+        t2.add(9);
+        System.out.println(t2);
+        t1.extend(t2);
+        System.out.println(t1);
+        System.out.println(t2);
     }
 }
