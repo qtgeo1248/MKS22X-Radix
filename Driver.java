73a62e96398134e8ba190f5af8e5abff3dc1fae8
qtgeo1248 <gzhou00@stuy.edu>
2019-04-01 18:49:51 -0400

    made more test dcases

--- Driver.java
+++ Driver.java
@@ -1,6 +1,17 @@
+import java.util.Arrays;
+
 public class Driver {
     public static void main(String[] args) {
         int[] test1 = {12, -31, 24, -42, 2, -1, 14, -21};
         Radix.radixsort(test1);
+        System.out.println(Arrays.toString(test1));
+
+        int[] test2 = {};
+        Radix.radixsort(test2);
+        System.out.println(Arrays.toString(test2));
+
+        int[] test3 = {614};
+        Radix.radixsort(test3);
+        System.out.println(Arrays.toString(test3));
     }
 }
