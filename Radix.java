7ab58c034739d111d1fcb5ee9cd723c921df605d
qtgeo1248 <gzhou00@stuy.edu>
2019-04-01 11:37:11 -0400

    forgot about neg ints

--- Radix.java
+++ Radix.java
@@ -1,12 +1,14 @@
+import java.lang.Math;
+
 public class Radix {
     public static void radixsort(int[] data) {
         if (data.length == 0) {
             return;
         }
-        int max = data[0];
+        int max = Math.abs(data[0]);
         for (int i = 1; i < data.length; i++) {
-            if (data[i] > max) {
-                max = data[i];
+            if (Math.abs(data[i]) > max) {
+                max = Math.abs(data[i]);
             }
         }
     }
